59ef42d6185fed372f8c60f625dc586eef1a6aa4
pcbaldwin <peter@egloo.ca>
2017-02-20 23:09:36 -0500

    Prepped for translations

--- language/en_US/simple_mode_lang.php
+++ language/en_US/simple_mode_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['simple_mode_app_name'] = 'Simple Mode';
 $lang['simple_mode_app_description'] = 'The Simple Mode app provides a standalone driver for the Central Management system.';
+$lang['simple_mode_app_name'] = 'Simple Mode';
