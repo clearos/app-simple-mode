2bf2d9aa1fb29fe218f7d1a65f46c863135dea0b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:20:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'De Simple Mode app biedt een stand-alone driver voor de Central Management systeem.';
+$lang['simple_mode_app_name'] = 'Simpele modus';
