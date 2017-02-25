4e52f0354c6d0a0e69000e1f854fe80d0f67cd9a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:14:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'အဆိုပါရိုးရှင်းသော Mode ကို app ကိုဗဟိုစီမံခန့်ခွဲမှုစနစ်အတွက်တစ်ဦးကို standalone ကားမောင်းသူပေးစွမ်းသည်။';
+$lang['simple_mode_app_name'] = 'ရိုးရှင်းသော Mode ကို';
