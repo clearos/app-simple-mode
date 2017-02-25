40da305626ab59b45211290bd99edc7f52645a9b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:23:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Дар барнома Усули Simple як ронандаи standalone барои системаи идораи марказии таъмин менамояд.';
+$lang['simple_mode_app_name'] = 'Усули оддӣ';
