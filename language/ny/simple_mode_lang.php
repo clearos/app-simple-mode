cc62ea60eaa157d9747f90b9d10f82465df1aaf6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:14:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'The Simple Mafilimu angaphunzitse app imapereka dalaivala standalone a dongosolo la Central Management.';
+$lang['simple_mode_app_name'] = 'Simple Mafilimu angaphunzitse';
