25027026a2775e33b4707d3c59454711093b174f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:20:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Il-Modalità app sempliċi jipprovdi mutur waħedha għas-Sistema ta &#39;ġestjoni ċentrali.';
+$lang['simple_mode_app_name'] = 'Modalità sempliċi';
