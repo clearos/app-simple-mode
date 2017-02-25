ce027817e9bc35f6fe9dcc0b90ace01478613100
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:24:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Просте додаток Mode забезпечує автономний драйвер для системи центрального управління.';
+$lang['simple_mode_app_name'] = 'простий режим';
