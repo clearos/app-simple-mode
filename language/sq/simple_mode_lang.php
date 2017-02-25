3332bcddd3d051967f7a052a3baedbcfe1c70e08
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:13:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'App Simple Mode siguron një shofer të pavarur për sistemin e Menaxhimit të qendrore.';
+$lang['simple_mode_app_name'] = 'Simple Mode';
