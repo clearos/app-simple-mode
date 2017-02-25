285e9b57f4c079c4d84c5e317c53a2abe83f2f29
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:13:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'The Simple Mode aplikazioa Central Kudeaketa sistema standalone gidari bat eskaintzen du.';
+$lang['simple_mode_app_name'] = 'Simple Mode';
