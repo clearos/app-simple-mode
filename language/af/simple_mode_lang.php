1d2880f3158cba701a986a9770f060aa266efb08
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:13:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Die eenvoudige modus app bied &#39;n selfstandige bestuurder vir die Sentrale Bestuur stelsel.';
+$lang['simple_mode_app_name'] = 'eenvoudige modus';
