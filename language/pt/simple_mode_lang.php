18ce2d5fae36250253a39d4a04d4c6bc5bd05b69
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:21:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'O aplicativo Simple Mode fornece um motorista independente para o sistema de gerenciamento central.';
+$lang['simple_mode_app_name'] = 'Modo simples';
