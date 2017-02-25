446a73d9feb6b9a11d4009e9d2a9dec5aa1a7950
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:19:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Paprasta režimas programa suteikia atskirą tvarkyklę Centrinės valdymo sistemos.';
+$lang['simple_mode_app_name'] = 'Paprasta režimas';
