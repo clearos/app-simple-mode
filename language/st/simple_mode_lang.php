2b58a2624ccecbc2f46df96fd15c1399f4d8ebe1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:22:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'The Simple Mode app e fana ka mokhanni standalone bakeng sa tsamaiso Central Management.';
+$lang['simple_mode_app_name'] = 'Simple Mode';
