6949980b7b87e6c47e6ee8ef7c242d03fe8c2c45
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:15:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Aplikacija Jednostavan način rada omogućuje samostalan program za sustav središnje uprave.';
+$lang['simple_mode_app_name'] = 'Jednostavan način rada';
