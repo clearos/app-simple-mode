1d2672ab823c6c231b46532aa37428d7b3892eb5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:24:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Các ứng dụng Chế độ đơn giản cung cấp một trình điều khiển độc lập cho hệ thống quản lý Trung ương.';
+$lang['simple_mode_app_name'] = 'Chế độ đơn giản';
