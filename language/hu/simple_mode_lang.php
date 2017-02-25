3c38e04cc842a7fd7a88f292c27b8e3cb46d7855
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:17:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'A Simple Mode alkalmazás biztosítja önálló driver a központi menedzsment rendszer.';
+$lang['simple_mode_app_name'] = 'egyszerű mód';
