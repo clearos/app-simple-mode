6d7e525653cee1f480c2f863e8f03228e9eaffcc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:20:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Aplikasi Simple Mode menyediakan pemandu yang berdiri sendiri untuk sistem Pengurusan Pusat.';
+$lang['simple_mode_app_name'] = 'Simple Mode';
