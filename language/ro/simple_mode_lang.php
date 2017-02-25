9f8103c7f0d867993888236a8f649598f419d716
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:21:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Aplicația simplă Mode oferă un driver pentru sine stătător sistemul de management central.';
+$lang['simple_mode_app_name'] = 'Mod simplu';
