4e4ccc2be31f188929df6d15a4f70404803dd78b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:17:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Soláthraíonn an app Mód Simplí tiománaí standalone don chóras um Bainistíocht Lárnach.';
+$lang['simple_mode_app_name'] = 'Mód simplí';
