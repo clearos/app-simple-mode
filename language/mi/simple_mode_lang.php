d51ef7f0a36d72edd6453dbb99f061b571341003
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:19:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'whakarato te taupānga Aratau Simple he taraiwa tūkotahi mo te pūnaha Whakahaere Central.';
+$lang['simple_mode_app_name'] = 'Aratau Simple';
