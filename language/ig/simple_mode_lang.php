a19ae463def892c7b7996e113a85dc61b04b8711
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:17:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'The Simple Mode ngwa na-enye a standalone ọkwọ ụgbọala maka Central Management usoro.';
+$lang['simple_mode_app_name'] = 'Simple mode';
