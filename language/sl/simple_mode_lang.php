6f7a45577d5b4353e7ad9cd58921a5b10d06672c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:22:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Aplikacija Simple način omogoča samostojno gonilnik za centralnega sistema za upravljanje.';
+$lang['simple_mode_app_name'] = 'preprost način';
