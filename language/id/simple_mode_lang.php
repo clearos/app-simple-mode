4352aa1cfe8fef7e51997f85ac00405293f4cbb3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:17:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Aplikasi Modus Sederhana menyediakan driver mandiri untuk sistem Manajemen Tengah.';
+$lang['simple_mode_app_name'] = 'Modus sederhana';
