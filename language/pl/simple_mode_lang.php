c00a2a6278bbc2e9a4ea413818b992100b4e8e14
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:21:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Aplikacja Tryb prosty pozwala na samodzielny sterownik dla Central Management System.';
+$lang['simple_mode_app_name'] = 'Prosty tryb';
