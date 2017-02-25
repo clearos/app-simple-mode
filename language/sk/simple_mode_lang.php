d9d811de258fea28ae12c0baf06222bd48b23a24
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:22:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Simple Mode aplikácia poskytuje samostatný ovládač pre systém centrálne vedenie.';
+$lang['simple_mode_app_name'] = 'jednoduchý režim';
