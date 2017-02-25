98133b5e7779d63cd9578841c196a4a80fde04af
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:18:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Қарапайым Mode Қолданба Орталық басқару жүйесі үшін оқшау драйверін қамтамасыз етеді.';
+$lang['simple_mode_app_name'] = 'қарапайым режимі';
