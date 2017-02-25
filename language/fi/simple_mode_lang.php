3da0016f9150a00dbee773df58fb5953c2541ff7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:15:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Yksinkertainen tila sovellus tarjoaa erillinen ohjain Keskitetty hallinta järjestelmään.';
+$lang['simple_mode_app_name'] = 'Simple Mode';
