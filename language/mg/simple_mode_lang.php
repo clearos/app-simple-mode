952c91fe51a193cc0cbdaa6f67a2bb862ce7376c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:19:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Ny kely saina dia manome Mode rindrambaiko standalone mpamily ny Central rafitra Management.';
+$lang['simple_mode_app_name'] = 'Simple Mode';
