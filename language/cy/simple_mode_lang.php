13bae0081ff3b2108b6ed24e695fd1f4435bd750
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:24:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/simple_mode_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['simple_mode_app_description'] = 'Mae&#39;r app Modd syml yn darparu gyrrwr standalone ar gyfer y system Rheoli Canolog.';
+$lang['simple_mode_app_name'] = 'Modd syml';
